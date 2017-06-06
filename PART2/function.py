f73499a8b2f290117e2ce9797f42cbe02b061564
Annamaria Sojnoczki <soj.annam@gmail.com>
2017-06-06 21:55:15 +0200

    added header to the column

--- PART2/function.py
+++ PART2/function.py
@@ -8,90 +8,79 @@ def get_config():
     return config
 
 
-def if_request_get(query):
+def database_connection(query):
     config = get_config()
     try:
         connect_str = "dbname={} user={} host='localhost' password={}".format(config[0], config[0], config[1])
         conn = psycopg2.connect(connect_str)
         conn.autocommit = True
+        cur = conn.cursor()
+        cur.execute(query)
+        column_header = [desc[0] for desc in cur.description]
+        table = cur.fetchall()
+        table = (list(map(list, table)))
+        column_head_upper = [x.upper() for x in [item for item in column_header]]
+        table.insert(0, column_head_upper)
+        return table
     except:
         print("I am unable to connect to the database")
-    cur = conn.cursor()
-    cur.execute(query)
-    table = cur.fetchall()
-    table = (list(map(list, table)))
-    return table
-
-
-def if_request_post(query):
-    config = get_config()
-    try:
-        connect_str = "dbname={} user={} host='localhost' password={}".format(config[0], config[0], config[1])
-        conn = psycopg2.connect(connect_str)
-        conn.autocommit = True
-    except:
-        print("I am unable to connect to the database")
-    cur = conn.cursor()
-    cur.execute(query)
-    conn.close()
-    return None
 
 
 def SQL_QUERY_show_mentors_and_schools():
-    mentors_and_schools = """SELECT mentors.first_name, mentors.last_name, schools.name, schools.country
+    mentors_and_schools = """SELECT mentors.first_name, mentors.last_name, schools.name AS schools, schools.country
                     FROM mentors
                     LEFT JOIN schools ON mentors.city=schools.city
                     ORDER BY mentors.id"""
-    return if_request_get(str(mentors_and_schools))
+    return database_connection(str(mentors_and_schools))
 
 
 def SQL_QUERY_select_schools():
     for key in request.args:
         condition = request.args.get(key)
-    select_schools = """SELECT mentors.first_name, mentors.last_name, schools.name, schools.country
+    select_schools = """SELECT mentors.first_name, mentors.last_name, schools.name AS Schools, schools.country
                     FROM mentors
                     LEFT JOIN schools ON mentors.city=schools.city
                     WHERE schools.name = '{}'
                     ORDER BY mentors.id""".format(condition)
-    return if_request_get(str(select_schools))
+    return database_connection(str(select_schools))
 
 
 def SQL_QUERY_select_country():
     for key in request.args:
         condition = request.args.get(key)
-    select_country = """SELECT  schools.country, COUNT(mentors.id) AS NumberOfMentors 
+    select_country = """SELECT  schools.country, COUNT(mentors.id) AS Number_Of_Mentors
                     FROM mentors
                     FULL OUTER JOIN schools ON mentors.city=schools.city
                     WHERE country= '{}'
                     GROUP BY country
                    ;""".format(condition)
-    return if_request_get(str(select_country))
+    return database_connection(str(select_country))
 
 
 def SQL_QUERY_show_all_schools():
-    all_schools = """SELECT mentors.first_name, mentors.last_name, schools.name, schools.country
+    all_schools = """SELECT mentors.first_name, mentors.last_name, schools.name As Schools, schools.country
                     FROM mentors
                     FULL OUTER JOIN schools ON mentors.city=schools.city
                     ORDER BY mentors.id"""
-    return if_request_get(str(all_schools))
+    return database_connection(str(all_schools))
 
 
 def SQL_QUERY_show_mentors_by_country():
-    mentors_by_country = """SELECT  schools.country, COUNT(mentors.id) AS NumberOfMentors
+    mentors_by_country = """SELECT  schools.country, COUNT(mentors.id) AS Number_Of_Mentors
                     FROM mentors
                     FULL OUTER JOIN schools ON mentors.city=schools.city
                     GROUP BY country
                     ORDER BY country
                    ;"""
-    return if_request_get(str(mentors_by_country))
+    return database_connection(str(mentors_by_country))
 
 
 def SQL_QUERY_show_contacts():
-    contacts = """SELECT   schools.name, mentors.first_name, mentors.last_name 
+    contacts = """SELECT   schools.name AS Schools, mentors.first_name, mentors.last_name
                     FROM mentors
                     INNER JOIN schools ON mentors.id=contact_person
                     ORDER BY name"""
-    return if_request_get(str(contacts))
+    return database_connection(str(contacts))
 
 
 def SQL_QUERY_show_applicants():
@@ -100,7 +89,7 @@ def SQL_QUERY_show_applicants():
                     LEFT JOIN applicants_mentors ON applicants.id=applicants_mentors.applicant_id
                     WHERE  creation_date >= make_date(2016,01,01)
                     ORDER BY creation_date DESC"""
-    return if_request_get(str(applicants))
+    return database_connection(str(applicants))
 
 
 def SQL_QUERY_show_all_applicants():
@@ -108,7 +97,7 @@ def SQL_QUERY_show_all_applicants():
                     FROM applicants
                     LEFT JOIN applicants_mentors ON applicants.id=applicants_mentors.applicant_id
                     ORDER BY creation_date DESC"""
-    return if_request_get(str(applicants))
+    return database_connection(str(applicants))
 
 
 def SQL_QUERY_show_select_applicants():
@@ -120,7 +109,7 @@ def SQL_QUERY_show_select_applicants():
                     LEFT JOIN applicants_mentors ON applicants.id=applicants_mentors.applicant_id
                     WHERE  creation_date >= make_date({},{},{})
                     ORDER BY creation_date DESC""".format(year, month, day)
-    return if_request_get(str(applicants))
+    return database_connection(str(applicants))
 
 
 def SQL_QUERY_show_applicants_and_mentors():
@@ -129,4 +118,4 @@ def SQL_QUERY_show_applicants_and_mentors():
                     FULL OUTER JOIN applicants_mentors ON applicants.id=applicants_mentors.applicant_id
                     LEFT JOIN mentors ON mentors.id = applicants_mentors.mentor_id
                     ORDER BY applicants.id"""
-    return if_request_get(str(applicants_and_mentors))
+    return database_connection(str(applicants_and_mentors))
--- PART2/main.py
+++ PART2/main.py
@@ -15,50 +15,50 @@ def main_page():
 def show_mentors_and_schools():
     '''Renders the List of all mentors with school and country'''
     mentors_and_schools = function.SQL_QUERY_show_mentors_and_schools()
-    return render_template('table.html', table=mentors_and_schools)
+    return render_template('mentors.html', table=mentors_and_schools)
 
 
 @app.route("/mentors/school", methods=["GET"])
 def show_one_schools_mentors():
     selected_school = function.SQL_QUERY_select_schools()
-    return render_template('table.html', table=selected_school)
+    return render_template('mentors.html', table=selected_school)
 
 
 @app.route('/all-schools', methods=["GET"])
 def show_all_schools():
     ''' Renders the List of all mentors with school and country, list include all the schools, even if there's no mentor yet!'''
     all_schools = function.SQL_QUERY_show_all_schools()
-    return render_template('table.html', table=all_schools)
+    return render_template('mentors.html', table=all_schools)
 
 
 @app.route('/mentors-by-country', methods=["GET"])
 def show_mentors_by_country():
     mentors_by_country = function.SQL_QUERY_show_mentors_by_country()
-    return render_template('table.html', table=mentors_by_country)
+    return render_template('mentors.html', table=mentors_by_country)
 
 
 @app.route('/mentors-by-country/country', methods=["GET"])
 def show_one_country():
     select_one_country = function.SQL_QUERY_select_country()
-    return render_template('table.html', table=select_one_country)
+    return render_template('mentors.html', table=select_one_country)
 
 
 @app.route('/contacts', methods=["GET"])
 def show_contacts():
     contacts = function.SQL_QUERY_show_contacts()
-    return render_template('table.html', table=contacts)
+    return render_template('mentors.html', table=contacts)
 
 
 @app.route('/applicants', methods=["GET"])
 def show_applicants():
     applicants = function.SQL_QUERY_show_applicants()
-    return render_template('table2.html', table=applicants)
+    return render_template('applicants.html', table=applicants)
 
 
 @app.route('/applicants/all', methods=["GET"])
 def show_all_applicants():
     all_applicants = function.SQL_QUERY_show_all_applicants()
-    return render_template('table2.html', table=all_applicants)
+    return render_template('applicants.html', table=all_applicants)
 
 
 @app.route('/applicants/select', methods=["POST"])
@@ -69,19 +69,18 @@ def show_select_applicants():
     print(year, month, day)
     print('ok')
     all_applicants = function.SQL_QUERY_show_select_applicants()
-    return render_template('table2.html', table=all_applicants)
+    return render_template('applicants.html', table=all_applicants)
 
 
 @app.route('/applicants-and-mentors', methods=["GET"])
 def show_applicants_and_mentors():
     applicants_and_mentors = function.SQL_QUERY_show_applicants_and_mentors()
-    return render_template('table2.html', table=applicants_and_mentors)
+    return render_template('applicants.html', table=applicants_and_mentors)
 
 
-""""
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('page_not_found.html'), 404"""
+    return render_template('404_error.html'), 404
 
 
 if __name__ == '__main__':
